4ac853ddd3e7ae105bae9ac3d65ac16af34c4326
Heloísa Medeiros <heloisam.silva@hotmail.com>
2016-04-28 20:39:01 -0300

    Arquivos do Projeto

---
+++ GroundCheck.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+    private Movimentação player;
+	// Use this for initialization
+	void Start () {
+        player = gameObject.GetComponentInParent<Movimentação>();
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D col) {
+        player.grounded = true;
+    }
+
+    void OnTriggerStay2D(Collider2D col) {
+        player.grounded = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        player.grounded = false;
+    }
+	
+}
+
